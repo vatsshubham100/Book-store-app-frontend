bc459408d30ed05fff0030387ebcdc7e63b3556d
Shubham Vats <124441513+vatsshubham100@users.noreply.github.com>
2024-07-28 11:03:42 +0530

    update1

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
 import axios from "axios";
 import { Link } from "react-router-dom";
+
 function Course() {
   const [book, setBook] = useState([]);
+  
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/book`);
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
@@ -16,11 +18,12 @@ function Course() {
     };
     getBook();
   }, []);
+
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
+          <h1 className="text-2xl md:text-4xl">
             We're delighted to have you{" "}
             <span className="text-pink-500"> Here! :)</span>
           </h1>
@@ -41,7 +44,7 @@ function Course() {
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
           {book.map((item) => (
-            <Cards key={item.id} item={item} />
+            <Cards key={item._id} item={item} />
           ))}
         </div>
       </div>
